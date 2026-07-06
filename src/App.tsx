edb3752739d669a98bec3a7d495d823665a5e92f
1235Google <souvikdashbbsr@gmail.com>
2026-07-06 16:44:04 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+export default function App() {
+  return (
+    <div>
+      <h1>My Website is Working 🚀</h1>
+    </div>
+  )
+}
